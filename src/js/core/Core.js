b9b6970f3992c79a2db2aee2660b6453acec4986
Alexander Sagalovskiy <spikesagal@users.noreply.github.com>
2014-03-28 12:07:29 -0400

    Moved console defs to core/Console.js

--- src/js/core/Core.js
+++ src/js/core/Core.js
@@ -11,26 +11,6 @@
  */ 
 (function() {
 
-	//// Console and Logging Configuration ////
-
-	// Old Browsers
-	if( typeof console == 'undefined' || !console ) console = {};
-	if( typeof console.log == 'undefined' || !console.log ) {
-		console.log = function() {};
-	}
-
-	// Newer IE
-	if( typeof console.debug == 'undefined' || !console.debug ) {
-		console.debug = console.log; //function() {};
-	}
-	if( typeof console.warn == 'undefined' || !console.warn) {
-		console.warn = console.log; //function() {};
-	}
-	if( typeof console.error == 'undefined' || !console.error ) {
-		console.error = console.log; //function() {};
-	}
-
-
 	//// Core Javascript extension ////
 
 	/*
